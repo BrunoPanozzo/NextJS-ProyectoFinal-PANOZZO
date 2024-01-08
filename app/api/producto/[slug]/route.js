1039a5b9faabc8c99a22e89b0120f213521e1e29
BrunoPanozzo <brunopanozzo@hotmail.com>
2024-01-08 17:05:33 -0300

    ajustes2

--- app/api/producto/[slug]/route.js
+++ app/api/producto/[slug]/route.js
@@ -3,30 +3,26 @@ import { db } from '@/firebase/config'
 import { doc, getDoc, onSnapshot } from "firebase/firestore"
 
 export async function GET(_, { params }) {
-    // const { slug } = params
-
-    // const docRef = doc(db, "productos", slug)
-    // // const docSnapshot = await getDoc(docRef)
-    // // return NextResponse.json(docSnapshot.data())
-
-    // //Uso onSnapshot para actualizaciones en tiempo real
-    // const unsubscribe = onSnapshot(docRef, (doc) => {
-    //     const responseData = doc.data()
-    //     return NextResponse.json(responseData)
-    // })
 
     try {
 
-    const { slug } = params
+        const { slug } = params
 
-    const docRef = doc(db, "productos", slug)
-    const docSnapshot = await getDoc(docRef)
+        const docRef = doc(db, "productos", slug)
 
-    if (docSnapshot.exists()) {
-        return NextResponse.json(docSnapshot.data());
-    } else {
-        return NextResponse.error(404, 'Product not found');
-    }
+        //Uso onSnapshot para actualizaciones en tiempo real
+        const unsubscribe = onSnapshot(docRef, (doc) => {
+            const responseData = doc.data()
+            return NextResponse.json(responseData)
+        })
+
+        // const docSnapshot = await getDoc(docRef)
+
+        // if (docSnapshot.exists()) {
+        //     return NextResponse.json(docSnapshot.data());
+        // } else {
+        //     return NextResponse.error(404, 'Product not found');
+        // }
 
     } catch (error) {
         console.error('Error fetching product:', error);
